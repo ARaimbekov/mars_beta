7ff8e396f4a3dce7f4c9efbfc2e69b63415b1f3a
araimbekov <raim.azim@mail.ru>
2023-12-11 14:40:12 +0700

    added get post method for api and get by id

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,21 +4,4 @@ from rest_framework import serializers
 class CdrSerializer(serializers.ModelSerializer):
     class Meta:
         model=Cdr
-        fields=(
-            'outbound_cid',
-            'src',
-            'dst',
-            'diversion',
-            'channel',
-            'dst_channel',
-            'start',
-            'answer',
-            'end',
-            'duration',
-            'billsec',
-            'disposition',
-            'uniquie_id',
-            'pbx',
-            'billsec',
-            'recpath',
-            )
+        fields=('__all__')
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import CdrCreateView
+from .views import CdrCreate, CdrList, CdrListById
 
 
 urlpatterns = [
-    path('get/', CdrCreateView.as_view()),
+    path('post/', CdrCreate.as_view()),
+    path('get/', CdrList.as_view()),
+    path('get/<int:pk>/', CdrListById.as_view())
 ]
