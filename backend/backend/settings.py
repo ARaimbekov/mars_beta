8866a374a7937814468c794186c6b0dbbf2d26f8
araimbekov <raim.azim@mail.ru>
2023-12-11 17:20:02 +0700

    debug = false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yr=b(69brx$xmop1d3!ob2oy86$(#cwp*g(0qo3b6jo57y5eu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
