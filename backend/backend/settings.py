f6665c165aeae0e393e2df6df03b57100577aaae
azim <raim.azim@mail.ru>
2024-01-28 23:02:00 +0700

    texting with docker + compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'postgres',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
@@ -155,7 +155,7 @@ LOGGING = {
         "api": {
             "level": "INFO",
             "class": "logging.FileHandler",
-            "filename": "/home/r2d2/mars_beta/api.log",
+            "filename": os.path.join(BASE_DIR, "api.log"),
         },
     },
     "loggers": {
